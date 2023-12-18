cfa5cf82b61eb279f020d86f5f1d3bb113ad3fa4
Sidiq Hersi <hersisidiq@gmail.com>
2023-12-18 17:58:47 +0000

    adds the questions in the questions array

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// includes packages needed for this application
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
@@ -5,7 +6,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'Github',
+      },
+      {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'Email',
+      },
+      {
+        type: 'input',
+        message: 'What is your project name?',
+        name: 'Project',
+      },
+      {
+        type: 'input',
+        message: 'Please write a short description of your project?',
+        name: 'Description',
+      },
+      {
+        type: 'Checkbox',
+        message: 'Please choose a license that is applicable for your project? (Use arrow key to select)',
+        name: 'License',
+        choices: ["MIT","Apache 2.0","GPL 3.0","BSD 3","None"]
+      },
+      {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'Install',
+      },
+      {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'Test',
+      },
+      {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'Repo',
+      },
+      {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'Contributing',
+      },
 ];
 
 // function to write README file
