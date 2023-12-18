3adafa891962cb662440145d6988be707b761dc8
Sidiq Hersi <hersisidiq@gmail.com>
2023-12-18 18:11:27 +0000

    adds line of code to two of the incomplete functions and installls inquirer

--- index.js
+++ index.js
@@ -56,11 +56,15 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
-
+inquirer.prompt(questions).then((response) => {
+console.log("Generating README File...");
+writeToFile("README.md", generateMarkdown({ ...response }));
+})
 }
 
 // function call to initialize program
